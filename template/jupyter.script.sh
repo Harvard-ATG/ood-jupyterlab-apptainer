b5335eb51cca43b265389cb824ba8a2d577e90da
Jeremy Guillette <3651377+jaguillette@users.noreply.github.com>
2024-09-09 15:59:58 -0400

    try using the config actually

--- template/jupyter.script.sh
+++ template/jupyter.script.sh
@@ -3,13 +3,4 @@
 # Run pyppeteer install to fix issue with exporting to pdf
 # pyppeteer-install
 
-jupyter lab \
-           --ip='0.0.0.0' \
-           --port="${MY_JUP_PORT}" \
-           --port-retries=0 \
-           --NotebookApp.password="${MY_JUP_PASSWD}" \
-           --NotebookApp.base_url="${MY_JUP_BASEURL}" \
-           --no-browser \
-           --NotebookApp.allow_origin='*' \
-           --NotebookApp.disable_check_xsrf=True \
-           --WebPDFExporter.disable_sandbox=True
+jupyter lab --config="${CONFIG_FILE}"
