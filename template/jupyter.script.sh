17203f540b6826757d9f1894e810c4715db6ea3e
Kevin Gray <kevin_gray@harvard.edu>
2026-02-02 02:04:24 -0500

    add lustre additions

--- template/jupyter.script.sh
+++ template/jupyter.script.sh
@@ -3,6 +3,21 @@
 # Run pyppeteer install to fix issue with exporting to pdf
 # pyppeteer-install
 
+# -------------------------------------------------------------------------
+# OPTIONAL: DISABLE HEAVY EXTENSIONS [DEACTIVATED]
+# -------------------------------------------------------------------------
+# CONTEXT:
+# The Jupyter Extension Manager attempts to fetch updates from the internet 
+# every time the notebook starts. This consumes significant CPU cycles (100% core usage)
+# for 2-4 seconds during startup.
+#
+# HOW TO ENABLE:
+# Uncomment the line below. This is recommended if running on small nodes 
+# (e.g., 1-2 CPUs) to prevent startup crashes or sluggishness.
+# -------------------------------------------------------------------------
+DISABLE_FLAGS=""
+# DISABLE_FLAGS="--LabApp.disabled_extensions='@jupyterlab/extensionmanager-extension'"
+
 jupyter lab \
            --ip='0.0.0.0' \
            --port="${MY_JUP_PORT}" \
